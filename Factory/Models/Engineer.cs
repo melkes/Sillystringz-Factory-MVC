725f817090ceeb53216f7d2fb43097fcb258bdad
Mike Wilkes <mikewilkes@outlook.com>
2023-03-25 07:25:51 -0700

    Corrections to README

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -6,6 +6,7 @@ namespace Factory.Models
   {
     public int EngineerId { get; set; }
     public string Name { get; set; }
+    public string EngineerDetails { get; set; }
     public List<Machine> Machines { get; set; }
   }
 }
